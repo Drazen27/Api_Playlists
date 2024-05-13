11628f58b2ddd6a77b2e57a0850ffd0b26362eaf
Drazen27 <103655456+Drazen27@users.noreply.github.com>
2024-05-13 13:42:06 -0400

    Commit con todo maso completo

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,35 @@ import swaggerDocument from "../swagger.json";
 var cors = require('cors')
 
 dotenv.config();
+// Configuración de CORS
 const corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200,
 };
 
+// Middleware personalizado para permitir CORS con las opciones definidas
+const allowCors = (req: Request, res: Response, next: () => void) => {
+  // Permite solicitudes desde cualquier origen
+  res.header('Access-Control-Allow-Origin', '*');
+  // Define otros encabezados permitidos
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  // Define métodos permitidos
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+};
+
+
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors(corsOptions));
+app.use(allowCors);
+
 app.use(express.json());
 
-app.use("/api",cors(),usersRoute);
+app.use("/api",cors(corsOptions),usersRoute);
 app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument))
 
 app.get("/", (req: Request, res: Response) => {
