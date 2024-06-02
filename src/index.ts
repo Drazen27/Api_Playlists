e54c51ea414339cbd52b8208d48cab31fa3bd8f3
Drazen27 <103655456+Drazen27@users.noreply.github.com>
2024-06-01 20:37:26 -0400

    Commit

--- src/index.ts
+++ src/index.ts
@@ -31,10 +31,6 @@ const allowCors = (req: Request, res: Response, next: () => void) => {
 };
 
 
-const options: swaggerUi.SwaggerUiOptions = {
-  customCss: CSS_URL,
-};
-
 const app = express();
 const port = process.env.PORT;
 //const port = 3000;
@@ -45,7 +41,12 @@ app.use(express.json());
 
 
 app.use("/api",cors(corsOptions),listaRoute);
-app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument,options))
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument,{
+  customCss:
+    '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  customCssUrl: CSS_URL,
+}));
+
 app.get("/swagger.json", (req: Request, res: Response) => {
   res.json(swaggerDocument);
 });
